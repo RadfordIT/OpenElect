34d9760fc5db26c4268d1b119457311c7ba43f3a
wenbang24 <wenbangalt@gmail.com>
2024-12-14 21:14:54 +1100

    refactor: moved colour config out

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('./config/colors.json')
 module.exports = {
     content: ["./templates/**/*.{html,tmpl}"],
     plugins: [
         require('@tailwindcss/typography'),
         require('daisyui'),
     ],
-    daisyui: {
-        themes: [
+    "daisyui": {
+        "themes": [
             {
                 ...require("daisyui/src/theming/themes")["light"],
-                primary: "blue",
-                secondary: "teal",
+                colors
             }
         ]
-    },
+    }
 }
-
