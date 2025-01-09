43128923e3b7809faee556fe1de4e3b88383594a
wenbang24 <wenbangalt@gmail.com>
2025-01-09 22:54:40 +1100

    results page

---
+++ results.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"slices"
+	"time"
+)
+
+func checkElectionEndedMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+		token := session.Get("user_id")
+		if token == nil {
+			c.String(http.StatusUnauthorized, "Unauthorized")
+			c.Abort()
+			return
+		}
+		groups := session.Get("groups").([]string)
+		if slices.Contains(groups, configEditor.GetString("admingroup")) {
+			c.Next()
+			return
+		}
+		endElectionTimeString := configEditor.GetString("endelectiontime")
+		endElectionTime, err := time.Parse("2006-01-02", endElectionTimeString)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "Error parsing election end time")
+			c.Abort()
+			return
+		}
+		if time.Now().Before(endElectionTime) {
+
+			c.String(http.StatusForbidden, "The election has not ended yet, results are not available.")
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
+
+func resultsRoutes() {
+	r.GET("/results", authMiddleware(), checkElectionEndedMiddleware(), func(c *gin.Context) {
+		c.HTML(http.StatusOK, "results.html", gin.H{
+			"candidates": "",
+		})
+	})
+}
