f0230df13fa77bb762ea231adfc7730b1cbc65b5
wenbang24 <wenbangalt@gmail.com>
2025-03-01 11:47:17 +1100

    change startup email address

--- email.go
+++ email.go
@@ -12,7 +12,7 @@ var emailClient *postmark.Client
 func emailSetup() {
 	emailClient = postmark.NewClient(os.Getenv("POSTMARK_SERVER_TOKEN"), os.Getenv("POSTMARK_ACCOUNT_TOKEN"))
 	email := postmark.Email{
-		From:     "0428079@radford.act.edu.au",
+		From:     "OpenElect Server <openelect@radford.act.edu.au>",
 		To:       "0358632@radford.act.edu.au",
 		Subject:  "OpenElect is Up",
 		TextBody: "OpenElect is up and running!",
