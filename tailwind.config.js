b361f03bea33c7088c98a4ec44c0b9b0edac8998
wenbang24 <wenbangalt@gmail.com>
2025-05-15 19:16:44 +1000

    migrated to tailwlind v4 and daisyui v5

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-
-const colors = require('./config/colors.json').colors;
-module.exports = {
-    content: ["./templates/**/*.{html,tmpl}", "./config/colors.json"],
-    plugins: [
-        require('@tailwindcss/typography'),
-    ],
-}
