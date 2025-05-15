b1cf30d18fb7fa2e5c6a6bdb5da237e30fcdccc7
wenbang24 <wenbangalt@gmail.com>
2025-05-15 19:10:33 +1000

    prepare for tailwind upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,34 +5,5 @@ module.exports = {
     content: ["./templates/**/*.{html,tmpl}", "./config/colors.json"],
     plugins: [
         require('@tailwindcss/typography'),
-        require('daisyui'),
     ],
-    daisyui: {
-        themes: [
-            {
-                mytheme: {
-                    "primary": colors.primary,
-                    "primary-content": colors.primaryContent,
-                    "secondary": colors.secondary,
-                    "secondary-content": colors.secondaryContent,
-                    "accent": colors.accent,
-                    "accent-content": colors.accentContent,
-                    "neutral": colors.neutral,
-                    "neutral-content": colors.neutralContent,
-                    "base-100": colors.base100,
-                    "base-200": colors.base200,
-                    "base-300": colors.base300,
-                    "base-content": colors.baseContent,
-                    "info": colors.info,
-                    "info-content": colors.infoContent,
-                    "success": colors.success,
-                    "success-content": colors.successContent,
-                    "warning": colors.warning,
-                    "warning-content": colors.warningContent,
-                    "error": colors.error,
-                    "error-content": colors.errorContent
-                }
-            }
-        ]
-    }
 }
